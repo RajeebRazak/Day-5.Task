d4d6094ca6ad51f7a967878a854c8e82dcea7e4a
RajeebRazak <145429667+RajeebRazak@users.noreply.github.com>
2023-09-30 15:39:27 +0530

    Update Question No 2.js

--- Question No 2.js
+++ Question No 2.js
@@ -11,32 +11,7 @@
         "year": 2021
       }
     ],
-    "experience": [
-      {
-        "position": "Software Enginee",
-        "company": "Tech Solutions Inc.",
-        "location": "Cityville, State",
-        "start_date": "2015-06-01",
-        "end_date": "2021-09-30",
-        "responsibilities": [
-          "Led a team of developers in the design and implementation of a scalable web application, resulting in a 30% increase in user engagement.",
-          "Collaborated with product managers to define project requirements and milestones, ensuring timely and successful deliveries.",
-          "Maintained and optimized a large-scale database, resulting in a 20% improvement in query performance."
-        ]
-      },
-      {
-        "position": "Software Engineer",
-        "company": "Code Innovators",
-        "location": "Techville, State",
-        "start_date": "2012-03-15",
-        "end_date": "2015-05-31",
-        "responsibilities": [
-          "Developed and maintained web applications using HTML, CSS, JavaScript, and Ruby on Rails.",
-          "Collaborated with cross-functional teams to gather and refine project requirements.",
-          "Participated in code reviews and provided mentorship to junior developers."
-        ]
-      }
-    ],
+   
     "skills": [
       "JavaScript",
       "HTML/CSS",
