8e5d188592870c7237ed35498fc51792f2e31022
RajeebRazak <145429667+RajeebRazak@users.noreply.github.com>
2023-09-30 15:31:22 +0530

    Add files via upload

---
+++ Question No 2.js
@@ -0,0 +1,66 @@
+{
+    "name": "Rajeeb Razak",
+    "email": "rajeebrazak786@gmail.com.com",
+    "phone": "8590808932",
+    "address": " ummini, dhoni Po. Palakkad, kerala, 678009",
+    "summary": "Experienced software engineer with a passion for developing innovative solutions. Proficient in various programming languages and technologies, with a strong background in web development and database management. Excellent problem-solving and communication skills.",
+    "education": [
+      {
+        "degree": "Diploma in mechanical engineering",
+        "university": "University of DOTE",
+        "year": 2021
+      }
+    ],
+    "experience": [
+      {
+        "position": "Software Enginee",
+        "company": "Tech Solutions Inc.",
+        "location": "Cityville, State",
+        "start_date": "2015-06-01",
+        "end_date": "2021-09-30",
+        "responsibilities": [
+          "Led a team of developers in the design and implementation of a scalable web application, resulting in a 30% increase in user engagement.",
+          "Collaborated with product managers to define project requirements and milestones, ensuring timely and successful deliveries.",
+          "Maintained and optimized a large-scale database, resulting in a 20% improvement in query performance."
+        ]
+      },
+      {
+        "position": "Software Engineer",
+        "company": "Code Innovators",
+        "location": "Techville, State",
+        "start_date": "2012-03-15",
+        "end_date": "2015-05-31",
+        "responsibilities": [
+          "Developed and maintained web applications using HTML, CSS, JavaScript, and Ruby on Rails.",
+          "Collaborated with cross-functional teams to gather and refine project requirements.",
+          "Participated in code reviews and provided mentorship to junior developers."
+        ]
+      }
+    ],
+    "skills": [
+      "JavaScript",
+      "HTML/CSS",
+      "Python",
+      "Java",
+      "Ruby on Rails",
+      "SQL",
+      "React",
+      "Node.js",
+      "Git",
+      "Agile/Scrum"
+    ],
+    "languages": [
+      "English (Fluent)",
+      "Malayalam (Fluent)"
+    ],
+    "certifications": [
+      "Certified ScrumMaster (CSM)",
+      "AWS Certified Developer - Associate"
+    ],
+    "interests": [
+      "Open-source projects",
+      "Web Development",
+      "Hiking"
+    ]
+  }
+  
